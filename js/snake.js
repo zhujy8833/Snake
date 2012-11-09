965b9923a62c60951b32a4a64a315a9661b81a88
jiangyue <jiangyue@regpacks.com>
2012-11-08 17:26:53 -0800

    start from scratch

---
+++ js/snake.js
@@ -0,0 +1,42 @@
+(function(){
+	var canvas = $('#playground')[0] || undefined,
+		context = canvas.getContext('2d'),
+		snake_array = [],
+		snake_color = "red";
+	
+	//functions	
+	var init = function(){
+		    context.fillStyle = "white";
+			context.fillRect()
+		
+		},
+	
+		paint = function(){
+		
+		},
+	
+		generate_snake = function(){
+			
+		}
+	
+})();
+
+/*
+var Snake = {
+	canvas : $('#playground')[0] || undefined,
+	snake_body : 5, //the initial snake length
+	context
+};
+
+Snake.generate_snake = function(){
+	
+}
+
+Snake.paint = function(){
+	
+}
+
+Snake.update_score = function(){
+	
+}
+*/
